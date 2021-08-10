3da295d73251e09040e7e1a6f426fd75eab06c30
jhonata <jhonatan229@outlook.com>
2021-08-10 16:08:18 -0300

    verify if size is into the param of the map before put into the char **

--- config_map.c
+++ config_map.c
@@ -6,7 +6,7 @@
 /*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 20:40:39 by jestevam          #+#    #+#             */
-/*   Updated: 2021/08/10 11:05:56 by jestevam         ###   ########.fr       */
+/*   Updated: 2021/08/10 16:06:23 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,17 @@
 #include "gnl/get_next_line.h"
 #include "libft/libft.h"
 
-static int	verify_number_column(char *c, t_map *map)
+static int	verify_number_column(char c, t_map *map)
 {
-	if (c[0] != '\n' && map->row_map == 0)
+	if (c != '\n' && map->row_map == 0)
 		map->base++;
-	if (c[0] != '\n')
+	if (c != '\n')
 		map->column_map++;
-	if (c[0] == '\n')
+	if (c == '\n')
 	{
 		//printf("base: %i, coumns %i\n", map->base, map->column_map);
 		if (map->base != map->column_map)
-		{
-			free(c);
 			return (-3);
-		}			
 		else
 			map->column_map = 0;
 		map->row_map++;
@@ -38,30 +35,28 @@ static int	verify_number_column(char *c, t_map *map)
 static int	count_lines(char *files, t_map *map)
 {
 	int		fd;
-	char	*c;
+	char	c;
 	int		rslt;
 
 	fd = open(files, O_RDONLY);
 	if (fd < 0)
 		return (fd);
-	c = malloc(sizeof(char) + 1);
-	if (c == NULL)
-		return (-4);
-	c[1] = 0;
-	rslt = read(fd, c, 1);
+	rslt = read(fd, &c, 1);
 	if (rslt < 0)
-		return (-2);
+		return (-1);
 	while (rslt > 0)
 	{
 		if (verify_number_column(c, map) < 0)
 			return (-3);
-		rslt = read(fd, c, 1);
+		rslt = read(fd, &c, 1);
 	}
-	if (map->column_map != 0)
-		map->row_map++;
-	printf("lines %i\n", map->row_map);
-	free(c);
 	close(fd);
+	if (map->base != map->column_map)
+		return (-3);
+	if (map->column_map < 3 || map->row_map < 3)
+		return (-3);
+	map->row_map++;
+	//printf("lines %i\n", map->row_map);
 	return (0);
 }
 
@@ -77,6 +72,7 @@ int	ft_read_map(char *file, t_map *map)
 	map->row_map = 0;
 	map->base = 0;
 	count = count_lines(file, map);
+	printf("count: %i\n", count);
 	if (count < 0)
 		return (count * -1);
 	fd = open(file, O_RDONLY);
@@ -84,11 +80,11 @@ int	ft_read_map(char *file, t_map *map)
 	index = get_next_line(fd, &map->map[index++]);
 	while (index < map->row_map)
 		get_next_line(fd, &map->map[index++]);
-	index = 0;
-	while (index < map->row_map)
-	{
-		printf("line %i:%10s\n", index + 1, map->map[index]);
-		index++;
-	}		
+	//index = 0;
+	//while (index < map->row_map)
+	//{
+	//	printf("line %i:%10s\n", index + 1, map->map[index]);
+	//	index++;
+	//}		
 	return (0);
 }
